c959168239b0ed4b43176fa9761ed6f0acd7ba2a
Alexis Thullier <thullier.alexis@hotmail.fr>
2025-07-03 09:37:40 +0200

    first init commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="./src/output.css" rel="stylesheet">
+  <title>ECF</title>
+</head>
+
+<body>
+  <h1 class="text-8xl">Hello World!</h1>
+</body>
+
+</html>
